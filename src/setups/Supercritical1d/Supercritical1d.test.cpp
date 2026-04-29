4d8539e07433f1e1db73b47aa35a16b150eb8bf0
Dominik Münch <muenchdomi610@gmail.com>
2026-04-29 12:06:57 +0200

    fix: change supercritical test values to match expected values

--- src/setups/Supercritical1d/Supercritical1d.test.cpp
+++ src/setups/Supercritical1d/Supercritical1d.test.cpp
@@ -3,45 +3,45 @@
  * @author Dominik Münch
  *
  * @section DESCRIPTION
- * Tests the subcritical setup.
+ * Tests the supercritical setup.
  **/
 #include <catch2/catch.hpp>
 #include "Supercritical1d.h"
 
-TEST_CASE( "Test the subcritical setup.", "[Subcritical1d]" ) {
-  tsunami_lab::setups::Subcritical1d l_subcritical;
+TEST_CASE( "Test the supercritical setup.", "[Supercritical1d]" ) {
+  tsunami_lab::setups::Supercritical1d l_supercritical;
 
 // testing within the hill
-  REQUIRE( l_subcritical.getHeight( 10, 0 ) == Approx(-0.13) );
+  REQUIRE( l_supercritical.getHeight( 10, 0 ) == Approx(0.13) );
 
-  REQUIRE( l_subcritical.getMomentumX( 10, 0 ) == Approx(0.18) );
+  REQUIRE( l_supercritical.getMomentumX( 10, 0 ) == Approx(0.18) );
 
-  REQUIRE( l_subcritical.getMomentumY( 10, 0 ) == Approx(0) );
+  REQUIRE( l_supercritical.getMomentumY( 10, 0 ) == Approx(0) );
 
-  REQUIRE( l_subcritical.getBathymetry( 10, 0 ) == Approx(-0.13) );
+  REQUIRE( l_supercritical.getBathymetry( 10, 0 ) == Approx(-0.13) );
 
-  REQUIRE( l_subcritical.getHeight( 10, 5 ) == Approx(0.13) );
+  REQUIRE( l_supercritical.getHeight( 10, 5 ) == Approx(0.13) );
 
-  REQUIRE( l_subcritical.getMomentumX( 10, 5 ) == Approx(0.18) );
+  REQUIRE( l_supercritical.getMomentumX( 10, 5 ) == Approx(0.18) );
 
-  REQUIRE( l_subcritical.getMomentumY( 10, 5 ) == Approx(0) );
+  REQUIRE( l_supercritical.getMomentumY( 10, 5 ) == Approx(0) );
 
-  REQUIRE( l_subcritical.getBathymetry( 10, 5 ) == Approx(-0.13) );
+  REQUIRE( l_supercritical.getBathymetry( 10, 5 ) == Approx(-0.13) );
 
 //testing at the part of flat bathymetry
-  REQUIRE( l_subcritical.getHeight( 2, 0 ) == Approx(0.33) );
+  REQUIRE( l_supercritical.getHeight( 2, 0 ) == Approx(0.33) );
 
-  REQUIRE( l_subcritical.getMomentumX( 2, 0 ) == Approx(0.18) );
+  REQUIRE( l_supercritical.getMomentumX( 2, 0 ) == Approx(0.18) );
 
-  REQUIRE( l_subcritical.getMomentumY( 2, 0 ) == Approx(0) );
+  REQUIRE( l_supercritical.getMomentumY( 2, 0 ) == Approx(0) );
 
-  REQUIRE( l_subcritical.getBathymetry( 2, 0 ) == Approx(-0.33) );
+  REQUIRE( l_supercritical.getBathymetry( 2, 0 ) == Approx(-0.33) );
 
-  REQUIRE( l_subcritical.getHeight( 2, 5 ) == Approx(0.33) );
+  REQUIRE( l_supercritical.getHeight( 2, 5 ) == Approx(0.33) );
 
-  REQUIRE( l_subcritical.getMomentumX( 2, 5 ) == Approx(-0.18) );
+  REQUIRE( l_supercritical.getMomentumX( 2, 5 ) == Approx(0.18) );
 
-  REQUIRE( l_subcritical.getMomentumY( 2, 5 ) == Approx(0) );
+  REQUIRE( l_supercritical.getMomentumY( 2, 5 ) == Approx(0) );
 
-  REQUIRE( l_subcritical.getBathymetry( 2, 5 ) == Approx(-0.33) );
+  REQUIRE( l_supercritical.getBathymetry( 2, 5 ) == Approx(-0.33) );
 }
