7e277b7743a4f16072805f06eb3999dd162dc675
dominik-mnch <muenchdomi610@gmail.com>
2026-05-15 20:07:18 +0200

    fix: use 100 as baseline bathymetry in artificial setup

--- src/setups/ArtificialTsunami2d/ArtificialTsunami2d.cpp
+++ src/setups/ArtificialTsunami2d/ArtificialTsunami2d.cpp
@@ -29,13 +29,13 @@ tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getDisplacement( t
 
 tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getHeight( t_real ,
                                                                 t_real  ) const {  
- return 20;
+ return 100;
 
 }
 
 tsunami_lab::t_real tsunami_lab::setups::ArtificialTsunami2d::getBathymetry( t_real i_x,
                                                                       t_real i_y ) const {
- return 20 + getDisplacement(i_x, i_y);
+ return -100 + getDisplacement(i_x, i_y);
 }
 
 
