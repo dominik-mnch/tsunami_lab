93b800f7a2a0b805c73519066994a3c729bf218a
dominik-mnch <muenchdomi610@gmail.com>
2026-06-13 11:45:49 +0200

    feat: optimize f wave solver by simplying math and flattening to one function call

--- src/solvers/F_wave.cpp
+++ src/solvers/F_wave.cpp
@@ -8,64 +8,6 @@
 #include "F_wave.h"
 #include <cmath>
 
-void tsunami_lab::solvers::F_wave::eigenvalues( t_real   i_hL,
-                                            t_real   i_hR,
-                                            t_real   i_uL,
-                                            t_real   i_uR,
-                                            t_real & o_waveSpeedL,
-                                            t_real & o_waveSpeedR ) {
-  // pre-compute square-root ops
-  t_real l_hSqrtL = std::sqrt( i_hL );
-  t_real l_hSqrtR = std::sqrt( i_hR );
-
-  // compute Roe averages
-  t_real l_hRoe = 0.5f * ( i_hL + i_hR );
-  t_real l_uRoe = l_hSqrtL * i_uL + l_hSqrtR * i_uR;
-  l_uRoe /= l_hSqrtL + l_hSqrtR;
-
-  // compute wave speeds
-  t_real l_ghSqrtRoe = m_gSqrt * std::sqrt( l_hRoe );
-  o_waveSpeedL = l_uRoe - l_ghSqrtRoe;
-  o_waveSpeedR = l_uRoe + l_ghSqrtRoe;
-}
-
-void tsunami_lab::solvers::F_wave::waveStrengths( t_real   i_hL,
-                                               t_real   i_hR,
-                                               t_real   i_huL,
-                                               t_real   i_huR,
-                                               t_real   i_bL,
-                                               t_real   i_bR,
-                                               t_real   i_waveSpeedL,
-                                               t_real   i_waveSpeedR,
-                                               t_real & o_strengthL,
-                                               t_real & o_strengthR ) {
-  // compute inverse of right eigenvector-matrix
-  t_real l_detInv = 1 / (i_waveSpeedR - i_waveSpeedL);
-
-  t_real l_rInv[2][2] = {{0}};
-  l_rInv[0][0] =  l_detInv * i_waveSpeedR;
-  l_rInv[0][1] = -l_detInv;
-  l_rInv[1][0] = -l_detInv * i_waveSpeedL;
-  l_rInv[1][1] =  l_detInv;
-
-  // compute jump in flux
-  t_real l_deltaF[2] = {0};
-  l_deltaF[0] = i_huR - i_huL;
-  l_deltaF[1] = (std::pow(i_huR,2) / i_hR  + 0.5 * std::pow(m_gSqrt,2) * std::pow(i_hR,2))
-                - (std::pow(i_huL,2) / i_hL  + 0.5 * std::pow(m_gSqrt,2) * std::pow(i_hL,2));
-
-  //respect bathymetry
-  t_real bathymetryTerm = -std::pow(m_gSqrt, 2)*(i_bR - i_bL) * ((i_hL + i_hR) / 2);
-  l_deltaF[1] = l_deltaF[1] - bathymetryTerm;
-
-  // compute wave strengths
-  o_strengthL  = l_rInv[0][0] * l_deltaF[0];
-  o_strengthL += l_rInv[0][1] * l_deltaF[1];
-
-  o_strengthR  = l_rInv[1][0] * l_deltaF[0];
-  o_strengthR += l_rInv[1][1] * l_deltaF[1];
-}
-
 void tsunami_lab::solvers::F_wave::netUpdates( t_real i_hL,
                                 t_real i_hR,
                                 t_real i_huL,
@@ -74,70 +16,60 @@ void tsunami_lab::solvers::F_wave::netUpdates( t_real i_hL,
                                 t_real i_bR,
                                 t_real o_netUpdateL[2],
                                 t_real o_netUpdateR[2] ) {
+  t_real l_g = m_gSqrt * m_gSqrt;
+  t_real l_gHalf = 0.5f * l_g;
 
  // compute particle velocities
   t_real l_uL = i_huL / i_hL;
   t_real l_uR = i_huR / i_hR;
 
   // compute wave speeds
-  t_real l_sL = 0;
-  t_real l_sR = 0;
+  t_real l_hSqrtL = std::sqrt( i_hL );
+  t_real l_hSqrtR = std::sqrt( i_hR );
+  t_real l_uRoe = (l_hSqrtL * l_uL + l_hSqrtR * l_uR) / (l_hSqrtL + l_hSqrtR);
+  t_real l_ghSqrtRoe = std::sqrt( l_gHalf * (i_hL + i_hR) );
+  t_real l_sL = l_uRoe - l_ghSqrtRoe;
+  t_real l_sR = l_uRoe + l_ghSqrtRoe;
 
-  eigenvalues( i_hL,
-              i_hR,
-              l_uL,
-              l_uR,
-              l_sL,
-              l_sR );
+  // compute jump in flux and bathymetry source term
+  t_real l_deltaF0 = i_huR - i_huL;
+  t_real l_deltaF1 = (i_huR * l_uR + l_gHalf * i_hR * i_hR)
+                   - (i_huL * l_uL + l_gHalf * i_hL * i_hL);
+  t_real l_bathymetryTerm = -l_gHalf * (i_bR - i_bL) * (i_hL + i_hR);
+  l_deltaF1 = l_deltaF1 - l_bathymetryTerm;
 
   // compute wave strengths
-  t_real l_aL = 0;
-  t_real l_aR = 0;
+  t_real l_detInv = 1 / (l_sR - l_sL);
+  t_real l_aL = (l_sR * l_deltaF0 - l_deltaF1) * l_detInv;
+  t_real l_aR = (l_deltaF1 - l_sL * l_deltaF0) * l_detInv;
 
-  waveStrengths( i_hL,
-                 i_hR,
-                 i_huL,
-                 i_huR,
-                 i_bL,
-                 i_bR,
-                 l_sL,
-                 l_sR,
-                 l_aL,
-                 l_aR );
+  t_real l_zL0 = l_aL;
+  t_real l_zL1 = l_aL * l_sL;
+  t_real l_zR0 = l_aR;
+  t_real l_zR1 = l_aR * l_sR;
 
-   //initialize waves
-   t_real l_Z1[2] = {0}; 
-   t_real l_Z2[2] = {0};
+  o_netUpdateL[0] = 0;
+  o_netUpdateL[1] = 0;
+  o_netUpdateR[0] = 0;
+  o_netUpdateR[1] = 0;
 
-   //calculate first wave
-   l_Z1[0] = l_aL;
-   l_Z1[1] = l_aL * l_sL;
+  // set net-updates depending on wave speeds
+  if( l_sL < 0 ) {
+    o_netUpdateL[0] += l_zL0;
+    o_netUpdateL[1] += l_zL1;
+  }
+  else if( l_sL > 0 ) {
+    o_netUpdateR[0] += l_zL0;
+    o_netUpdateR[1] += l_zL1;
+  }
 
-   //calculate second wave
-   l_Z2[0] = l_aR;
-   l_Z2[1] = l_aR * l_sR;
-
-   // set net-updates depending on wave speeds
-  for( unsigned short l_qt = 0; l_qt < 2; l_qt++ ) {
-    // init
-    o_netUpdateL[l_qt] = 0;
-    o_netUpdateR[l_qt] = 0;
-
-    // 1st wave
-    if( l_sL < 0 ) {
-      o_netUpdateL[l_qt] += l_Z1[l_qt];
-    }
-    else if (l_sL > 0) {
-      o_netUpdateR[l_qt] += l_Z1[l_qt];
-    }
-
-    // 2nd wave
-    if( l_sR > 0 ) {
-      o_netUpdateR[l_qt] += l_Z2[l_qt];
-    }
-    else if (l_sR < 0) {
-      o_netUpdateL[l_qt] += l_Z2[l_qt];
-    }
+  if( l_sR > 0 ) {
+    o_netUpdateR[0] += l_zR0;
+    o_netUpdateR[1] += l_zR1;
+  }
+  else if( l_sR < 0 ) {
+    o_netUpdateL[0] += l_zR0;
+    o_netUpdateL[1] += l_zR1;
   }
 
 }
--- src/solvers/F_wave.test.cpp
+++ src/solvers/F_wave.test.cpp
@@ -6,87 +6,7 @@
  * Unit tests of the F-wave solver.
  **/
 #include <catch2/catch.hpp>
-#define private public
 #include "F_wave.h"
-#undef public
-
-TEST_CASE( "Test the derivation of the eigenvalues.", "[Eigenvalues]" ) {
-   /*
-    * Test case:
-    *  h: 4 | 10.3
-    *  u: 8 | -2
-    *
-    * roe height: 7.15
-    * roe velocity: (sqrt(4) * 8 + (-2) * sqrt(10.3)) / ( sqrt(4) + sqrt(10.3) )
-    *               = 1.8392422450039443
-    * roe speeds: s1 = 1.8392422450039443 - sqrt(9.80665 * 7.15) = -6.5343798805
-    *             s2 = 1.8392422450039443 + sqrt(9.80665 * 7.15) = 10.2128643705
-    */
-  float l_waveSpeedL = 0;
-  float l_waveSpeedR = 0;
-  tsunami_lab::solvers::F_wave::eigenvalues( 4,
-                                         10.3,
-                                         8,
-                                         -2,
-                                         l_waveSpeedL,
-                                         l_waveSpeedR );
-
-  REQUIRE( l_waveSpeedL == Approx(  -6.5343798805 ) );
-  REQUIRE( l_waveSpeedR == Approx(  10.2128643705 ) );
-}
-
-TEST_CASE( "Test the derivation of the Roe wave strengths.", "[RoeStrengths]" ) {
-  /*
-   * Test case:
-   *  h:   4 | 10.3
-   *  u:   8 | -2
-   *  hu: 32 |-20.6
-   *
-   * The derivation of the Roe speeds (s1, s2) is given above.
-   *
-   *  Matrix of right eigenvectors:
-   *
-   *      | 1   1 |
-   *  R = |       |
-   *      | s1 s2 |
-   *
-   * Inversion yields:
-   *
-   * wolframalpha.com query: invert {{1, 1}, {-6.5343798805, 10.2128643705}}
-   *
-   *         |  0.60982357559  -0.059711316382 |
-   * R_inv = |                                 |
-   *         |  0.39017642441   0.059711316382 |
-   *
-   * Multiplicaton with the jump in flux gives the wave strengths:
-   *
-   * wolframalpha.com query: {{0.60982357559, -0.059711316382},
-   *                          {0.39017642441, 0.059711316382}} 
-   *                        *  {-20.6 - 32,
-                                (-20.6)^2 / 10.3  + 0.5 * 3.131557121^2 * 10.3^2
-                                - (32^2 /  4    + 0.5 * 3.131557121^2 * 4^2 ) }
-   *
-   *        |                                               -20.6 - 32                                       |   | -45.6276 |
-   * Rinv * |                                                                                                | = |          |
-   *        | (-20.6)^2 / 10.3 + 0.5 * 3.131557121^2 * 10.3^2 - (32^2) /  4    + 0.5 * 3.131557121^2 * 4^2 ) |   | -6.97236 |
-   */
-  float l_strengthL = 0;
-  float l_strengthR = 0;
-
-  tsunami_lab::solvers::F_wave::waveStrengths( 4,
-                                            10.3,
-                                            32,
-                                            -20.6,
-                                            0,
-                                            0,
-                                            -6.5343798805,
-                                            10.2128643705,
-                                            l_strengthL,
-                                            l_strengthR );
-
-  REQUIRE( l_strengthL == Approx( -45.6276 ) );
-  REQUIRE( l_strengthR == Approx( -6.97236 ) );
-}
 
 TEST_CASE( "Test the derivation of the F wave net-updates.", "[FWaveUpdates]" ) {
   /*
@@ -215,69 +135,6 @@ TEST_CASE( "Test the derivation of the F wave net-updates.", "[FWaveUpdates]" ) 
   REQUIRE( l_netUpdatesR[1] == Approx(0) );
 }
 
-TEST_CASE( "Test the derivation of the F-wave strengths with bathymetry.", "[FWaveStrengthsBathymetry]" ) {
-  /*
-   * Test case (bathymetry effect on wave strengths):
-   *
-   *     left | right
-   *   h:  10 | 10
-   *  hu:   0 |  0
-   *   b:  -2 | -4
-   *
-   * Roe height:   (10 + 10) / 2 = 10
-   * Roe velocity: 0 (both velocities are zero)
-   * Roe speeds:
-   *   s1 = 0 - sqrt(9.80665 * 10) = -sqrt(98.0665) ≈ -9.902853
-   *   s2 = 0 + sqrt(9.80665 * 10) =  sqrt(98.0665) ≈  9.902853
-   *
-   *   wolframalpha.com query: sqrt(9.80665 * 10)
-   *
-   * Jump in flux (without bathymetry):
-   *   deltaF[0] = 0 - 0 = 0
-   *   deltaF[1] = (0/10 + 0.5 * 9.80665 * 10^2) - (0/10 + 0.5 * 9.80665 * 10^2) = 0
-   *
-   * Bathymetry source term:
-   *   psi = -9.80665 * (bR - bL) * (hL + hR) / 2
-   *       = -9.80665 * (-4 - (-2)) * 10
-   *       = -9.80665 * (-2) * 10
-   *       = 196.133
-   *
-   *   adjusted deltaF[1] = 0 - 196.133 = -196.133
-   *
-   * Inversion of eigenvector matrix:
-   *
-   *   wolframalpha.com query: invert {{1, 1}, {-9.902853, 9.902853}}
-   *
-   *          |  0.5    -0.050490 |
-   *   R_inv =|                   |    where 0.050490 = 1 / (2 * 9.902853)
-   *          |  0.5     0.050490 |
-   *
-   * Wave strengths (multiplication with adjusted jump in flux):
-   *
-   *   wolframalpha.com query: {{0.5, -0.050490}, {0.5, 0.050490}} * {0, -196.133}
-   *
-   *          |  0.5 * 0 + (-0.050490) * (-196.133) |   |  9.902853 |
-   *   alpha = |                                      | = |           |
-   *          |  0.5 * 0 + ( 0.050490) * (-196.133) |   | -9.902853 |
-   */
-  float l_strengthL = 0;
-  float l_strengthR = 0;
-
-  tsunami_lab::solvers::F_wave::waveStrengths( 10,
-                                               10,
-                                               0,
-                                               0,
-                                               -2,
-                                               -4,
-                                               -9.902853,
-                                               9.902853,
-                                               l_strengthL,
-                                               l_strengthR );
-
-  REQUIRE( l_strengthL == Approx(  9.902853 ) );
-  REQUIRE( l_strengthR == Approx( -9.902853 ) );
-}
-
 TEST_CASE( "Test the derivation of the F-wave net-updates with bathymetry.", "[FWaveUpdatesBathymetry]" ) {
   /*
    * Test case (bathymetry effect on net-updates):
@@ -287,8 +144,7 @@ TEST_CASE( "Test the derivation of the F-wave net-updates with bathymetry.", "[F
    *  hu:   0 |  0
    *   b:  -2 | -4
    *
-   * The derivation of the Roe speeds and wave strengths is given above
-   * (see [FWaveStrengthsBathymetry]):
+  * The Roe speeds and wave strengths are:
    *   s1 ≈ -9.902853,  s2 ≈ 9.902853
    *   alpha1 ≈ 9.902853, alpha2 ≈ -9.902853
    *
