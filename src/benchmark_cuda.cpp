a98480a30e93493890662f65e7304000fb920f47
Dominik Münch <muenchdomi610@gmail.com>
2026-07-06 11:23:05 +0200

    fix: snprintf

--- src/benchmark_cuda.cpp
+++ src/benchmark_cuda.cpp
@@ -539,7 +539,7 @@ namespace {
 
           // unique tag of nvtx3
           char rangeName[64];
-          sprintf(rangeName, sizeof(rangeName), "Bench_%d_%s_%d", l_size.first, l_layoutSpec.name, l_rep); 
+          snprintf(rangeName, sizeof(rangeName), "Bench_%d_%s_%d", l_size.first, l_layoutSpec.name, l_rep); 
 
           nvtxRangePushA(rangeName);
 
